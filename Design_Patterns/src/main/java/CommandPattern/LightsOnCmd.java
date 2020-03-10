10ceb859d28e413f41ae64ad4cfff347dc7df5a0
ravitejakolla04 <ravitejakolla04@gmail.com>
2020-03-10 16:59:55 +0530

    Initial Commit

---
+++ Design_Patterns/src/main/java/CommandPattern/LightsOnCmd.java
@@ -0,0 +1,12 @@
+package CommandPattern;
+
+public class LightsOnCmd implements Command{
+	Light light;
+	public LightsOnCmd(Light light){
+		this.light = light;
+	}
+	public void execute(){
+		light.switchOn();
+		System.out.println("Light on");
+	}
+}
